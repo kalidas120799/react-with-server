184360f07f6b1d29680103051376188545849a69
kalidas120799 <s.kalidas120799@gmail.com>
2021-09-21 11:39:03 +0530

    server dotenv config & client set default port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
-const port = 3001 || process.env.PORT
+const path=require("path")
+const dotenv=require("dotenv").config({path:path.resolve(__dirname,"./.env")})
+const port = process.env.PORT || 3002
 var app = express()
 
 app.use(express.json())
@@ -15,10 +17,12 @@ var usersData = require("./database/users.json")
 
 app.get("/", (req, res) => { return res.send("okay") })
 
-app.get("/users", (req, res) => { 
-    const {token}=req.headers
+console.log(process.env.TEST)
+
+app.get("/users", (req, res) => {
+    const { token } = req.headers
     console.log(token)
     return res.json(usersData)
- })
+})
 
 app.listen(port, () => { console.log(`App Running on http://localhost:${port}`) })
